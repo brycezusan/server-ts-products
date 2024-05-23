b09f26568002e15499785d80affbf6600f75b83a
brizusan <brycezusan@gmail.com>
2024-05-23 13:01:17 -0500

    update db

--- src/config/db.ts
+++ src/config/db.ts
@@ -11,7 +11,7 @@ const db = new Sequelize(`${process.env.DATABASE_URL}`,
         require: false,
       },
     },
-    models:[__dirname + "/../models/**/*.ts"]
+    models:[__dirname + "/../models/**/*"]
   }
 );
 
